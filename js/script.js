70442791583152d8db35391ea12d5912f0b8fc2f
Mladja <mladenlukic111@gmail.com>
2024-09-03 18:26:29 +0200

    Populated HTML, added functionality for New and Featured buttons.

--- js/script.js
+++ js/script.js
@@ -1,28 +1,57 @@
-
 let dataList = [];
-
 
 // Fetching data from data.json file
 
 const fetchData = async () => {
-    try {
-        const response = await fetch("data.json");
-        const data = await response.json();
+  try {
+    const response = await fetch("data.json");
 
-        data.forEach(item => {
-            let newItem = {
-                ...item,
-            };
-            dataList.push(newItem);
-        });
+    if (!response.ok) throw new Error("Network response was not ok");
 
-    } catch (error) {
-        console.error("Error due to fetching data:", error);
-    }
+    const data = await response.json();
+
+    dataList = data.map((item) => ({
+      ...item,
+      categories: [item.role, item.level, ...item.languages],
+    }));
+  } catch (error) {
+    console.error("Error due to fetching data:", error);
+  }
 };
 
-// console.log(dataList, typeof(dataList));
+let jobsContainer = document.querySelector(".jobs-container");
+
+// Function that populates HTML
+const populateHTML = async () => {
+  await fetchData();
+
+  dataList.forEach((job) => {
+    // Checking if object has new and featured key value true or not
+    const hasNew = job.new === true;
+    const newButton = hasNew ? `<button class='new'>New!</button>` : "";
+
+    const hasFeatured = job.featured === true;
+    
+
+    const featuredButton = hasFeatured
+      ? `<button class='new'>Featured!</button>`
+      : "";
+
+    jobsContainer.innerHTML += `<div class='jobs-wrapper'>
+                                  <div class='content'>
+                                    <div class='image-wrapper'><img src='${job.logo}' alt=''/></div>
+                                    <div class='description'>
+                                      <div class='first-row'><span class='company'>${job.company}</span><span>${newButton}</span><span>${featuredButton}</span></div>
+                                      <div class='second-row'><p>${job.position}</p></div>
+                                      <div class='third-row'><span>${job.postedAt}</span>&#x2022;<span>${job.contract}</span>&#x2022;<span>${job.location}</span></div>
+                                    </div>
+                                  </div>
+                                  <div class='categories'></div>
+                                </div>`;
+  });
+};
 
 // Calling function for fetching data
 fetchData();
 
+populateHTML();
